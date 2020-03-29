11a02cbc237c794433163ddab79c31456fd028f2
Nadun De Silva <nadun.eranga.baduge@accenture.com>
2020-03-29 10:24:53 +0800

    Initial Commit

---
+++ ImageSearchApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ImageSearchApp
+//
+//  Created by nadun.eranga.baduge on 29/3/20.
+//  Copyright © 2020 nadun.eranga.baduge. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageSearchApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ImageSearchApp
+//
+//  Created by nadun.eranga.baduge on 29/3/20.
+//  Copyright © 2020 nadun.eranga.baduge. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
